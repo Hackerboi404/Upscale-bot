c15d9cdb54989e8fa9112e3148e68985ce43097d
Hackerboi404 <tuturahul14rk@gmail.com>
2026-04-16 01:24:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 # Plain text token hata do yahan se
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-REPLICATE_API_TOKEN = os.environ.get("REPLICATE_API_TOKEN")
+HF_TOKEN = os.environ.get("HF_TOKEN")
 
 PORT = int(os.environ.get("PORT", 5000))

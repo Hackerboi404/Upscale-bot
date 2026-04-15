f07f0289a02a08b7b2b120af7da9c5e1d3a6c23d
Hackerboi404 <tuturahul14rk@gmail.com>
2026-04-16 01:32:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-# Plain text token hata do yahan se
+# Render ke 'Environment' tab mein ye keys add karna zaroori hai
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 HF_TOKEN = os.environ.get("HF_TOKEN")
 
+# Render automatically PORT deta hai, default 5000
 PORT = int(os.environ.get("PORT", 5000))

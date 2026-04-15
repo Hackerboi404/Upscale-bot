c6001a78ff6c07add5565c862ffa8b08877f78ef
Hackerboi404 <tuturahul14rk@gmail.com>
2026-04-16 02:06:54 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Render ke 'Environment' tab mein ye keys add karna zaroori hai
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-HF_TOKEN = os.environ.get("HF_TOKEN")
-
-# Render automatically PORT deta hai, default 5000
-PORT = int(os.environ.get("PORT", 5000))

59b6cf050026afa434a3ebdb2380db3bc4143604
Hackerboi404 <tuturahul14rk@gmail.com>
2026-04-16 00:57:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = "TUMHARA_TELEGRAM_BOT_TOKEN"
+REPLICATE_API_TOKEN = "r8_epG**********************************"
+
+# Render automatically PORT environment variable deta hai
+PORT = int(os.environ.get("PORT", 5000))
+
